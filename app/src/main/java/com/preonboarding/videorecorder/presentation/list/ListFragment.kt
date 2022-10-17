b8ac65fb5b56483602992c52304443856327a451
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-10-17 17:06:16 +0900

    [SET] 뷰모델 세팅

--- app/src/main/java/com/preonboarding/videorecorder/presentation/list/ListFragment.kt
+++ app/src/main/java/com/preonboarding/videorecorder/presentation/list/ListFragment.kt
@@ -1,7 +1,13 @@
 package com.preonboarding.videorecorder.presentation.list
 
+import androidx.fragment.app.activityViewModels
 import com.preonboarding.videorecorder.R
 import com.preonboarding.videorecorder.databinding.FragmentListBinding
+import com.preonboarding.videorecorder.presentation.MainViewModel
 import com.preonboarding.videorecorder.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class ListFragment : BaseFragment<FragmentListBinding>(R.layout.fragment_list)
+@AndroidEntryPoint
+class ListFragment : BaseFragment<FragmentListBinding>(R.layout.fragment_list) {
+    private val listViewModel: MainViewModel by activityViewModels()
+}
--- app/src/main/java/com/preonboarding/videorecorder/presentation/play/PlayFragment.kt
+++ app/src/main/java/com/preonboarding/videorecorder/presentation/play/PlayFragment.kt
@@ -1,7 +1,13 @@
 package com.preonboarding.videorecorder.presentation.play
 
+import androidx.fragment.app.activityViewModels
 import com.preonboarding.videorecorder.R
 import com.preonboarding.videorecorder.databinding.FragmentPlayBinding
+import com.preonboarding.videorecorder.presentation.MainViewModel
 import com.preonboarding.videorecorder.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class PlayFragment : BaseFragment<FragmentPlayBinding>(R.layout.fragment_play)
+@AndroidEntryPoint
+class PlayFragment : BaseFragment<FragmentPlayBinding>(R.layout.fragment_play) {
+    private val playVideModel: MainViewModel by activityViewModels()
+}
