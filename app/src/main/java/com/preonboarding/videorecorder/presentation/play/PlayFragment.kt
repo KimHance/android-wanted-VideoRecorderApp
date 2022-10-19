3e6fde52857baf86c3c9329df72fe3ae86f389aa
gksgpd97 <gksgpd97@naver.com>
2022-10-19 20:03:24 +0900

    [FEAT] 영상 재생 화면 기본 구현 완료

--- app/src/main/java/com/preonboarding/videorecorder/presentation/play/PlayFragment.kt
+++ app/src/main/java/com/preonboarding/videorecorder/presentation/play/PlayFragment.kt
@@ -1,5 +1,6 @@
 package com.preonboarding.videorecorder.presentation.play
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
@@ -8,8 +9,12 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 import com.preonboarding.videorecorder.R
 import com.preonboarding.videorecorder.databinding.FragmentPlayBinding
+import com.preonboarding.videorecorder.domain.model.Video
 import com.preonboarding.videorecorder.presentation.MainViewModel
 import com.preonboarding.videorecorder.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,21 +25,28 @@ class PlayFragment : BaseFragment<FragmentPlayBinding>(R.layout.fragment_play) {
     private val playVideoModel: MainViewModel by activityViewModels()
     private val navArgs: PlayFragmentArgs by navArgs()
 
+    private val testUri =
+        "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+    private val testVideo = Video("", testUri)
+    private var uri: String = testUri
+    private var exoPlayer: ExoPlayer? = null
+    private var exoPlayWhenReady = true
+    private var exoCurrentWindow = 0
+    private var exoPlaybackPosition = 0L
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // 선택된 비디오 세팅
-        //getSelectedVideo()
-        collectFlow()
-        initView()
+        binding.fragment = this@PlayFragment
+        getSelectedVideo()
     }
 
-    private fun initView() {
-        binding.apply {
-            // TODO 네비게이션 설정 되면 붙히세요
-            // 안그러면 터집니다.
-            /*ivPlayBack.setOnClickListener {
-                requireView().findNavController().popBackStack()
-            }*/
+    private fun setExoPlayer() {
+        val mediaItem = MediaItem.fromUri(uri)
+        exoPlayer = ExoPlayer.Builder(requireContext()).build().also {
+            binding.pvPlayVideo.player = it
+            it.setMediaItem(mediaItem)
+            it.playWhenReady = exoPlayWhenReady
+            it.seekTo(exoCurrentWindow, exoPlaybackPosition)
+            it.prepare()
         }
     }
 
@@ -42,13 +54,44 @@ class PlayFragment : BaseFragment<FragmentPlayBinding>(R.layout.fragment_play) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 playVideoModel.selectedVideo.collect { video ->
-                    // TODO 선택된 비디오 ExoPlayer와 작업 필요
+                    uri = video.uri
+                    setExoPlayer()
                 }
             }
         }
     }
 
     private fun getSelectedVideo() {
-        playVideoModel.setSelectedVideo(navArgs.video)
+        //playVideoModel.setSelectedVideo(navArgs.video)
+        playVideoModel.setSelectedVideo(testVideo)
+        collectFlow()
+    }
+
+    fun setBackButton(view: View) {
+        requireView().findNavController().popBackStack()
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.run {
+            exoPlaybackPosition = this.currentPosition
+            exoCurrentWindow = this.currentMediaItemIndex
+            exoPlayWhenReady = this.playWhenReady
+            release()
+        }
+        exoPlayer = null
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
     }
 }
